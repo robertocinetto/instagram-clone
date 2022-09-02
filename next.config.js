646f0fbe3ecf20cee2fa5579a960ad4af9ccc036
Roberto Cinetto <roberto.cinetto@gmail.com>
2022-09-01 18:17:49 -0700

    IG logos in header

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['cdn.iphoneincanada.ca', 'upload.wikimedia.org'],
+  },
 }
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -10,7 +10,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Hello World</h1>
+      {/* Header */}
+      <Header />
+
+      {/* Header */}
+
+      {/* Header */}
     </div>
   )
 }
