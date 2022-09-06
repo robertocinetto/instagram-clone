c3b6573e0dd592f09333e8e7069361dcf2bba695
Roberto Cinetto <roberto.cinetto@gmail.com>
2022-09-06 15:07:34 -0700

    Signin page completed

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,7 +1,6 @@
 import { getProviders, signIn } from 'next-auth/react'
 import Header from '../../components/Header'
 export default function signin({ providers }) {
-  console.log(providers)
   return (
     <>
       <Header />
@@ -37,7 +36,7 @@ export default function signin({ providers }) {
   )
 }
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps() {
   const providers = await getProviders()
   return {
     props: { providers },
